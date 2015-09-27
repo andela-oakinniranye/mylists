11a75698dbf985aa320d75e183fa7c9b12e1b8f0
Oreoluwa Akinniranye <oreoluwa.akinniranye@andela.com>
2015-09-27 22:59:25 +0100

    renamed application to have consistent name

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -16,4 +16,4 @@
 # Using secret_token for rails3 compatibility. Change to secret_key_base
 # to avoid deprecation warning.
 # Can be safely removed in a rails3 api-only application.
-Mylist::Application.config.secret_token = '0d4c675b77df20f14e40340b849f17f68638163c0f9d2810a4725a83e45b74b2552d19c8281728d60c3ba4874b3443df2fb099812177ff01fe5247158d7db553'
+Mylyst::Application.config.secret_token = '0d4c675b77df20f14e40340b849f17f68638163c0f9d2810a4725a83e45b74b2552d19c8281728d60c3ba4874b3443df2fb099812177ff01fe5247158d7db553'
